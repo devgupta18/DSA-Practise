00f1054c0c1de71736735bbe2711260410eddeda
devgupta18 <gupta.dev2002@gmail.com>
2023-05-06 23:08:37 +0530

    OOPs Concept

---
+++ oops.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class oops {
+    char a;
+    float p;
+    char b;
+    int num;
+};
+
+int main() {
+    oops o1;
+    cout<<sizeof(o1);
+    return 0;
+}
